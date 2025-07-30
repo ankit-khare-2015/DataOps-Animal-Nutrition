6e64a1b63ec190bd93b6d87666f2338738dc0ede
Ankit Khare <ankit.khare.2015@gmail.com>
2025-07-30 11:16:29 +0200

    Reorder SQL SELECT statement for improved readability

--- dbt_project/models/marts/mart_feed_cost_efficiency.sql
+++ dbt_project/models/marts/mart_feed_cost_efficiency.sql
@@ -1,11 +1,11 @@
 SELECT
-    af.feed_id,
-    af.animal_type,
-    af.feed_name,
-    af.region,
-    af.protein / NULLIF(af.price_per_kg, 0) AS protein_per_dollar,
-    af.energy / NULLIF(af.price_per_kg, 0) AS energy_per_dollar,
-    rp.climate_zone,
-    rp.avg_rainfall_mm
-  FROM {{ ref('stg_animal_feed') }} af
-LEFT JOIN {{ ref('dim_region') }} rp ON af.region = rp.region
+  af.feed_id,
+  af.animal_type,
+  af.feed_name,
+  af.region,
+  rp.climate_zone,
+  rp.avg_rainfall_mm,
+  af.protein / NULLIF(af.price_per_kg, 0) AS protein_per_dollar,
+  af.energy / NULLIF(af.price_per_kg, 0) AS energy_per_dollar
+FROM {{ ref('stg_animal_feed') }} AS af
+LEFT JOIN {{ ref('dim_region') }} AS rp ON af.region = rp.region
